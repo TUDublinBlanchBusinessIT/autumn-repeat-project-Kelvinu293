0a0789f78ae94f3486e4f95fe70aac6475bf3aa7
Kelvinu293 <b00141682@mytudublin.ie>
2025-08-12 21:21:10 +0100

    changing

--- createDBV1.sql
+++ createDBV1.sql
@@ -9,3 +9,23 @@ CREATE TABLE events (
     event_date DATE
 );
 
+USE event_manager;
+
+CREATE TABLE attendees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(50),
+  lastname VARCHAR(50),
+  email VARCHAR(120) UNIQUE,
+  type VARCHAR(20)  
+);
+
+USE event_manager;
+
+CREATE TABLE bookings (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  event_id INT NOT NULL,
+  attendee_id INT NOT NULL,
+  registered_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (event_id) REFERENCES events(id),
+  FOREIGN KEY (attendee_id) REFERENCES attendees(id)
+);
