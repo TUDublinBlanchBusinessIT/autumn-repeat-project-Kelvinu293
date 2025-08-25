98d1d3fe422e9b4620e0480fec91d9f121cbf0ba
Kelvinu293 <b00141682@mytudublin.ie>
2025-08-25 19:43:44 +0100

    changing

--- createDBV1.sql
+++ createDBV1.sql
@@ -26,6 +26,8 @@ CREATE TABLE bookings (
   event_id INT NOT NULL,
   attendee_id INT NOT NULL,
   registered_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  start_Time TIME,
+  end_Time TIME,
   FOREIGN KEY (event_id) REFERENCES events(id),
   FOREIGN KEY (attendee_id) REFERENCES attendees(id)
 );
