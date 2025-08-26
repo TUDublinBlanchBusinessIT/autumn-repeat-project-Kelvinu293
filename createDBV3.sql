50d6d3bb87eab05736d7275363ff67d61f42a86d
Kelvinu293 <b00141682@mytudublin.ie>
2025-08-26 03:05:56 +0100

    change in dbv3 bookings

--- createDBV3.sql
+++ createDBV3.sql
@@ -4,13 +4,14 @@ CREATE TABLE bookings (
   id INT AUTO_INCREMENT PRIMARY KEY,
   event_id INT NOT NULL,
   attendee_id INT NOT NULL,
-  registered_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  arrange INT,
+  people INT,
   start_Time TIME,
   end_Time TIME,
   FOREIGN KEY (event_id) REFERENCES events(id),
   FOREIGN KEY (attendee_id) REFERENCES attendees(id)
 );
 
-Insert into bookings (event_id, attendee_id, start_time, end_time) values (1, 1, '02:00:00', '17:00:00');
-Insert into bookings (event_id, attendee_id, start_time, end_time) values (1, 2, '18:00:00', '20:00:00');
-Insert into bookings (event_id, attendee_id, start_time, end_time) values (2, 1, '10:00:00', '18:00:00');
+Insert into bookings (event_id, attendee_id, arrange, people, start_time, end_time) values (1, 1, 4, 50, '02:00:00', '17:00:00');
+Insert into bookings (event_id, attendee_id, arrange, people, start_time, end_time) values (1, 2, 3, 75, '18:00:00', '20:00:00');
+Insert into bookings (event_id, attendee_id, arrange, people, start_time, end_time) values (2, 1, 1, 21, '10:00:00', '18:00:00');
